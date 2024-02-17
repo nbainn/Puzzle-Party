6fab1f006de5abc187a4fe1def10e0105e9cfc0e
Arielle Dong <112361805+Arielle8219@users.noreply.github.com>
2024-02-17 13:44:43 -0500

    connection

--- backend/server/client.js
+++ backend/server/client.js
@@ -1,3 +1,9 @@
+const { connect } = require("http2");
+
+// Central host id 
+var hostID;
+
+
 // Define the loadScript function
 function loadScript(url, callback) {
     const script = document.createElement('script');
@@ -11,15 +17,57 @@ function loadScript(url, callback) {
 const libraryUrl = 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js';
 loadScript(libraryUrl, () => {
     // The library has been loaded, you can now use it
-    const peer = new Peer();
+
     
     // Function that uses the Peer object
-    function myFunction() {
+    function createHost() {
+        const peer = new Peer();
         peer.on('open', (id) => {
             console.log('My peer ID is: ' + id);
+            hostID = id
+        });
+        peer.on('connection', function(conn) {
+            conn.on('open', function() {
+                // Receive messages
+                conn.on('data', function(data) {
+                console.log('Received', data);
+                });
+
+                // Send messages
+                conn.send('Hello!');
+            });
+        });
+
+
+    }
+
+    function createPeer() {
+        const peer = new Peer();
+        var peerID
+        peer.on('open', (id) => {
+            console.log('My peer ID is: ' + id);
+            peerID = id
+        });
+        connectToPeer(hostID)
+
+    }
+ 
+    // Function to establish a connection to another peer
+    function connectToPeer(peerId) {
+        const conn = peer.connect(peerId);
+    
+        conn.on('open', function() {
+        console.log('Connected to peer ' + peerId);
+        // Now you can send data through the connection
+        conn.send('Hello from client!');
+        });
+    
+        conn.on('data', function(data) {
+        console.log('Received data from peer ' + peerId + ':', data);
         });
     }
-    
     // Call the function
-    myFunction();
+  
 });
+
+export default loadScript;
