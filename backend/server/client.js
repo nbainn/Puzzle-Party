ee59a4241260e90db0600f4b2595853d7d18c6b9
Arielle Dong <112361805+Arielle8219@users.noreply.github.com>
2024-02-17 12:55:00 -0500

    fixing errors

--- backend/server/client.js
+++ backend/server/client.js
@@ -1,5 +1,25 @@
-var peer = new Peer();
+// Define the loadScript function
+function loadScript(url, callback) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onload = callback;
+    document.head.appendChild(script);
+}
 
-peer.on('open', function(id) {
-	console.log('My peer ID is: ' + id);
-  });
+// Load the external library
+const libraryUrl = 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js';
+loadScript(libraryUrl, () => {
+    // The library has been loaded, you can now use it
+    const peer = new Peer();
+    
+    // Function that uses the Peer object
+    function myFunction() {
+        peer.on('open', (id) => {
+            console.log('My peer ID is: ' + id);
+        });
+    }
+    
+    // Call the function
+    myFunction();
+});
