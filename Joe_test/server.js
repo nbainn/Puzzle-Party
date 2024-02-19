b0358ff775e49c421bdefd5b49a5dd5e78976956
joeruhe <jogiruhe@gmail.com>
2024-02-18 20:19:37 -0500

    Working Server with peer to peer basic chat
    Created a server to self-host peerJS and do all of the work connecting peers. This server also serves html, and users can connect to other users by typing in their peer id, they can also send messages once they are connected.

--- Joe_test/server.js
+++ Joe_test/server.js
@@ -1,30 +1,48 @@
-const express = require('../config/node_modules/express');
+const express = require("../config/node_modules/express");
+//const cors = require("../config/node_modules/cors");
+const http = require("http");
+const path = require("path");
 const app = express();
-const path = require('path');
 const port = 3000;
 
-// Enable CORS for all routes
+//app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
+// Serve static files from the 'config' directory
+app.use(express.static(path.join(__dirname, "../config")));
+
 // Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route handler for the root path
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Endpoint to handle GET requests
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Hello from the server!" });
 });
 
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+const { ExpressPeerServer } = require("../config/node_modules/peer");
+
+const peerServer = ExpressPeerServer(server, {
+  proxied: true,
+  debug: true,
+  path: "/myapp",
+  ssl: {},
+});
+
+app.use(peerServer);
