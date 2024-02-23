d7f540432ebb9438740d3497b202cdfa459bfc86
nltoosicks <87550604+nltoosicks@users.noreply.github.com>
2024-02-22 23:51:40 -0500

    Complete frontend overhaul; installed ReactJS, Implemented basic frontend functionality (not finished)

---
+++ frontend/src/components/ClueList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ClueList.css'; 
+
+function ClueList() {
+  // ClueList implementation goes here
+  return (
+    <div className="clue-list">
+      {/* Clues display */}
+    </div>
+  );
+}
+
+export default ClueList;
---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Grid.css';
+
+function Grid() {
+  // Grid implementation goes here
+  return (
+    <div className="crossword-container">
+      {/* Crossword grid */}
+    </div>
+  );
+}
+
+export default Grid;
