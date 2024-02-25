b391ff151b378bc1c9202ee5d4a9b8d578b7cbe3
joeruhe <jogiruhe@gmail.com>
2024-02-25 11:12:39 -0500

    Added a proper config.js file
    Used to store port number and base url, so that if someone wished to change where the server is run they only have to change in one place!

--- Joe_test/server.js
+++ Joe_test/server.js
@@ -1,57 +1,31 @@
 // Express framework
 const express = require("../node_modules/express");
-// Might not need http and path
+// Importing peer server capabilities
+const { ExpressPeerServer } = require("../node_modules/peer");
+const config = require("../config.js");
+// Express will import http automatically too
 const http = require("http");
 const path = require("path");
 const app = express();
 const port = 3000;
 
-// Used to make live server somewhat compatible, mostly garbage
-// const cors = require("../config/node_modules/cors");
-/*app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});*/
-
-// Serve static files from the 'public' folder
-// What this does is allow server.js to grab index.html (or other files)
-// from the public folder and give them to the client
-// This also puts all files in the public folder on the same level as server.js,
-// so you can access index.html by typing localhost:3000/index.html instead of localhost:3000/public/index.html
+// Allows server to serve react build files
 app.use(express.static(path.join(__dirname, "build")));
 
-// Route handler for the root path
-// When the webpage is opened, this is the initial http request
-// The index.html file is sent to the client so they can render it
-// req stands for request, and res stands for response
-// The single "/" represents the root directy of the server, which is
-// the initial request sent
+// Root of the webpage, serves the react build to be displayed
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
-// If you were to go to localhost:3000/api/data (arbitrary path), then the server
-// would return this instead of an html file
-// Used for getting information from the server, or new webpages
+// Sample http request to the server
 app.get("/api/data", (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
-// Start the server at the port given above and listens for incoming connections
-// Initial connection are at the root directory, which we have set to serve the html above
-const server = app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Listening for http requests on port 3000
+const server = app.listen(config.PORT, "0.0.0.0", () => {
+  console.log(`Server running at ${config.BASE_URL}:${port}`);
 });
-
-// This grabs the ExpressPeerServer object from peer library (using relative path as
-// noted by lack of inital forward slash)
-// require is like import
-const { ExpressPeerServer } = require("../node_modules/peer");
 
 // This lets the peer JS server run on the same port as local host because we can define a path
 // denoted by /myapp (arbitrary)
@@ -63,6 +37,5 @@ const peerServer = ExpressPeerServer(server, {
   ssl: {},
 });
 
-// Using peerServer
-// app is express embodied
+// Using peerServer to handle peerJS requests through defined path
 app.use(peerServer);
---
+++ config.js
@@ -0,0 +1,11 @@
+// Used to store the base URL of the website...
+// ...so that it can be easily changed if the website is hosted elsewhere.
+const BASE_URL = "http://localhost";
+const PORT = 3000;
+
+module.exports = {
+  BASE_URL,
+  PORT,
+};
+
+// Import this in all files that need the base URL
