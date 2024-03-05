6d10ab19c6f695d449fb67c22961aedc56938deb
joeruhe <jogiruhe@gmail.com>
2024-03-05 12:32:19 -0500

    Reorganized file structure
    Backend holds server/sequelize file, Frontend hold react files. All other test files can be found under the Test_folders

---
+++ Test_folders/Nate_test/src/components/RoomPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import ChatBox from './ChatBox';
+import ClueList from './ClueList';
+import Grid from './Grid';
+import PlayerList from './PlayerList';
+import './RoomPage.css'; // Importing CSS for RoomPage
+
+function RoomPage() {
+  const { roomId } = useParams();
+
+  return (
+    <div className="room-page">
+      <div className="room-header">
+        <h2>Room: {roomId}</h2>
+        {/* You can also display the room code here */}
+      </div>
+      <div className="game-container">
+        <PlayerList />
+        <Grid />
+        <div className="hints-chat-container">
+          <ClueList />
+          <ChatBox />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RoomPage;
