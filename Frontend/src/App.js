56125997968086305649e963d0c57f0ee4797fa2
joeruhe <jogiruhe@gmail.com>
2024-03-11 18:53:52 -0400

    fixed merge conflict

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RoomPage from './components/RoomPage';
-<<<<<<< HEAD
-import PublicRooms from './components/PublicRooms';
-=======
-import CrosswordGrid from './components/Crossword';
->>>>>>> 427cf5c2ff44a5641db69a89c90e915520a0ec54
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RoomPage from "./components/RoomPage";
+import PublicRooms from "./components/PublicRooms";
+import CrosswordGrid from "./components/Crossword";
 
 function App() {
   return (
@@ -14,11 +11,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/room/:roomId" element={<RoomPage />} />
-<<<<<<< HEAD
         <Route path="/rooms/" element={<PublicRooms />} />
-=======
-        <Route path = "/crossword" element = {<CrosswordGrid/>} />
->>>>>>> 427cf5c2ff44a5641db69a89c90e915520a0ec54
+        <Route path="/crossword" element={<CrosswordGrid />} />
       </Routes>
     </Router>
   );
