1155f69e9deb097eb8531fe59a57aa56bf881312
joeruhe <jogiruhe@gmail.com>
2024-03-24 14:07:41 -0400

    smoother puzzle generation with randomization

--- Frontend/src/components/GeneratePuzzleForm.js
+++ Frontend/src/components/GeneratePuzzleForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./generatePuzzleForm.css";
+
 //import {fetchHost} from '../sequelize.tsx';
 //import {sq} from '../sequelize.tsx';
 import e from "cors";
@@ -8,41 +9,67 @@ import axios from "axios";
 
 function GeneratePuzzleForm() {
   const [seed, setSeed] = useState("");
+  const [size, setSize] = useState("medium");
+
   const navigate = useNavigate();
+
+  const handleSizeChange = (event) => {
+    setSize(event.target.value);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     // TODO: Add validation for roomCode before redirecting\
-    try {
-      const response = await axios.post("/puzzle", { seed });
-      if (response.status === 200) {
-        console.log("Puzzle Received", response.data);
-      } else if (response.status === 404) {
-        console.log("Error", response.data);
-      } else {
-        console.error("Unexpected response status:", response.status);
+    if (seed !== "") {
+      try {
+        const response = await axios.post("/puzzle", { seed, size });
+        if (response.status === 200) {
+          console.log("Puzzle Received", response.data);
+        } else if (response.status === 404) {
+          console.log("Error", response.data);
+        } else {
+          console.error("Unexpected response status:", response.status);
+        }
+      } catch (error) {
+        console.error("Error contacting server", error);
+        console.log("error");
       }
-    } catch (error) {
-      console.error("Error contacting server", error);
-      console.log("error");
     }
   };
 
+  const generateRandomNumber = () => {
+    // Generate a random number between 0 and 9999999999
+    const randomNumber = Math.floor(Math.random() * 10000000000);
+
+    // Set the generated random number to the input field
+    setSeed(randomNumber.toString());
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="generate-puzzle-form">
-      <input
-        type="text"
-        placeholder="Enter Puzzle Seed"
-        value={seed}
-        onChange={(e) => setSeed(e.target.value)}
-        minLength="6"
-        maxLength="10"
-        className="generate-puzzle-input"
-      />
-      <button type="submit" className="generate-puzzle-button">
-        Generate Puzzle
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className="generate-puzzle-form">
+        <input
+          id="seedInput"
+          type="number"
+          placeholder="Enter Puzzle Seed"
+          value={seed}
+          onChange={(e) => setSeed(e.target.value)}
+          min="0"
+          max="9999999999"
+          className="generate-puzzle-input"
+        />
+        <button type="submit" className="generate-puzzle-button">
+          Generate Puzzle
+        </button>
+      </form>
+      <button onClick={generateRandomNumber}>Randomize</button>
+      <label htmlFor="sizeDropdown">&nbsp;&nbsp;Select Size:</label>
+      <select id="sizeDropdown" value={size} onChange={handleSizeChange}>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+      </select>
+    </div>
   );
 }
 
--- Frontend/src/components/RoomPage.js
+++ Frontend/src/components/RoomPage.js
@@ -6,9 +6,8 @@ import Grid from "./Grid";
 import PlayerList from "./PlayerList";
 import ExitRoom from "./ExitRoom";
 import RoomStatus from "./RoomStatus";
-import GeneratePuzzleForm from "./generatePuzzleForm";
-import DropdownComponent from "./DropdownComponent";
-import { useAuth } from '../hooks/useAuth';
+import GeneratePuzzleForm from "./GeneratePuzzleForm";
+import { useAuth } from "../hooks/useAuth";
 import "./RoomPage.css";
 
 function RoomPage() {
@@ -19,34 +18,41 @@ function RoomPage() {
   useEffect(() => {
     if (ablyClient) {
       // Log the current connection state
-      console.log('Current Ably connection state:', ablyClient.connection.state);
-  
+      console.log(
+        "Current Ably connection state:",
+        ablyClient.connection.state
+      );
+
       // Set up a listener for when the connection state changes
       const onConnectionStateChange = (stateChange) => {
-        console.log('Ably connection state has changed:', stateChange.current);
-        
+        console.log("Ably connection state has changed:", stateChange.current);
+
         // If Ably is connected, update state accordingly
-        if (stateChange.current === 'connected') {
-          console.log('Ably is now connected.');
+        if (stateChange.current === "connected") {
+          console.log("Ably is now connected.");
           setAblyReady(true);
         } else {
-          console.log('Ably is not connected. Current state:', stateChange.current);
+          console.log(
+            "Ably is not connected. Current state:",
+            stateChange.current
+          );
           setAblyReady(false);
         }
       };
-  
-      ablyClient.connection.on('connectionstate', onConnectionStateChange);
-  
+
+      ablyClient.connection.on("connectionstate", onConnectionStateChange);
+
       // Immediately invoke the callback if already connected
-      if (ablyClient.connection.state === 'connected') {
+      if (ablyClient.connection.state === "connected") {
         setAblyReady(true);
       }
-  
+
       // Clean up listener when the component is unmounted
-      return () => ablyClient.connection.off('connectionstate', onConnectionStateChange);
+      return () =>
+        ablyClient.connection.off("connectionstate", onConnectionStateChange);
     }
   }, [ablyClient]);
-  
+
   // Use the 'ablyReady' state to control your loading screen
   if (!ablyReady) {
     return <div>Loading...</div>;
@@ -56,19 +62,23 @@ function RoomPage() {
     <div className="room-page">
       <div>
         <ExitRoom />
-        <RoomStatus roomId={roomId}/>
+        <RoomStatus roomId={roomId} />
       </div>
       <div className="room-header">
         <h2>Room: {roomId}</h2>
         <GeneratePuzzleForm />
-        <DropdownComponent />
       </div>
       <div className="game-container">
         <PlayerList />
         <Grid />
         <div className="hints-chat-container">
           <ClueList />
-          <ChatBox ablyClient={ablyClient} roomId={roomId} userId={userId} userColor={userColor} />
+          <ChatBox
+            ablyClient={ablyClient}
+            roomId={roomId}
+            userId={userId}
+            userColor={userColor}
+          />
         </div>
       </div>
     </div>
