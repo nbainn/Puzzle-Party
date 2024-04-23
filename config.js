a4151b9f2adc424b41d58b61a2268a10cf8ef09b
nltoosicks <87550604+nltoosicks@users.noreply.github.com>
2024-04-23 17:48:42 -0400

    New Port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Used to store the base URL of the website...
 // ...so that it can be easily changed if the website is hosted elsewhere.
 const BASE_URL = "http://puzparty.store";
-const PORT = 3000;
+const PORT = 80;
 module.exports = {
   BASE_URL,
   PORT,
